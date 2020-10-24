940198b8f855270fc89d154f94534bdea7f7d4bb
Chathura <71864409+Chathura-ce@users.noreply.github.com>
2020-10-24 10:59:20 +0530

    Before Chapter  - 4

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    @{{user.username}} - {{fullName}}
+    <strong>followers:</strong>{{followers}}
+    <button @click="followUser">
+      follow
+    </button>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: '@chathura',
+        firstName: 'chathura',
+        lastName: 'edirisinghe',
+        email: 'abc@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  watch:{
+    followers(newFollowerCount, oldFollowerCount){
+      if (oldFollowerCount < newFollowerCount){
+        console.log(`${this.user.username} has gained a follower!`)
+      }
+    }
+  },
+  computed:{
+   fullName(){
+     return `${this.user.firstName} ${this.user.lastName}`
+   }
+  },
+  methods:{
+    followUser(){
+      this.followers++
+    }
+  },
+  mounted() {
+    this.followUser();
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
